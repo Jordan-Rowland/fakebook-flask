20591678d6b1d534c90e6b6b26b3ada0f64828fd
Jordan Rowland <JordanRowland00@gmail.com>
2019-02-14 21:07:35 -0800

    adding timestamp to posts, trying to update the visual elements. Should probably save for later

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,10 @@
 import os
 
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -17,4 +18,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 db = SQLAlchemy(app)
+moment = Moment(app)
 Migrate(app, db)
