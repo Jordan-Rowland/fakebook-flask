4891baf9a6b50f56fd3a279c93455741dae5c74a
Jordan Rowland <JordanRowland00@gmail.com>
2019-02-25 08:26:06 -0800

    Added follows table to database, need to set up ignores, and add this to the interface

--- app/models.py
+++ app/models.py
@@ -13,6 +13,20 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer(), db.ForeignKey('users.id'), primary_key=True)
+    followed_id = db.Column(db.Integer(), db.ForeignKey('users.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+# class Follow(db.Model):
+#     __tablename__ = 'follows'
+#     follower_id = db.Column(db.Integer(), db.ForeignKey('users.id'), primary_key=True)
+#     followed_id = db.Column(db.Integer(), db.ForeignKey('users.id'), primary_key=True)
+#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
@@ -28,6 +42,16 @@ class User(db.Model, UserMixin):
     about_me = db.Column(db.Text(), default=None)
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    followed = db.relationship('Follow',
+        foreign_keys=[Follow.follower_id],
+        backref=db.backref('follower', lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+    followers = db.relationship('Follow',
+        foreign_keys=[Follow.followed_id],
+        backref=db.backref('followed', lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
 
 
     def __init__(self, email, username, 
@@ -72,6 +96,30 @@ class User(db.Model, UserMixin):
         db.session.commit()
 
 
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+
+    def is_following(self, user):
+        if user.id is None():
+            return False
+        return self.followed.filter_by(follower_id=user.id).first() is not None
+
+
+    def is_followed_by(self, user):
+        if user.id is None():
+            return False
+        return self.followers.filter_by(follower_id=user.id).first() is not None
+
+
 class Post(db.Model):
     __tablename__ = 'posts'
 
@@ -89,4 +137,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.id}', '{self.timestamp}', '{self.user_id}')"
-
